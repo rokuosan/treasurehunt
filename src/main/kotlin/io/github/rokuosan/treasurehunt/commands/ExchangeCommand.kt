bcef1d33f7b19a3acaada7c65217f2994a110484
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-10-22 20:53:00 +0900

    fix: limit the time to exchange

--- src/main/kotlin/io/github/rokuosan/treasurehunt/commands/ExchangeCommand.kt
+++ src/main/kotlin/io/github/rokuosan/treasurehunt/commands/ExchangeCommand.kt
@@ -2,6 +2,7 @@ package io.github.rokuosan.treasurehunt.commands
 
 import io.github.rokuosan.treasurehunt.TreasureHunt
 import io.github.rokuosan.treasurehunt.inventories.ExchangeInventory
+import io.github.rokuosan.treasurehunt.utils.GameResource
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -12,6 +13,12 @@ class ExchangeCommand: CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
         if (!command.name.equals("exchange", true)) return false
         if (sender !is Player) return false
+
+        val status = GameResource.status
+        if (status != GameResource.GameStatus.PLAYING) {
+            sender.sendMessage("The game is not started")
+            return true
+        }
 
         val plugin = TreasureHunt.plugin
 
